340852c33073eedd07eb0696aa0b55014a106cb8
unknown <alex.meyer62@gmail.com>
2019-01-04 14:06:23 -0800

    If snapshot is already in progress, skip snapshot.

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -15,6 +15,10 @@ def filter_instances(project):
         instances = ec2.instances.all()
     return instances
 
+def has_pending_snapshot(volume):
+    snapshots = list(volume.snapshots.all())
+    return snapshots and snapshots[0].state == 'pending'
+
 @click.group()
 def cli():
     """Shotty manages snapshots"""
@@ -46,7 +50,7 @@ def list_snapshots(project, list_all):
                 )))
             
                 if s.state == 'completed' and not list_all: break
-                    
+
     return 
 
 @cli.group('volumes')
@@ -93,8 +97,12 @@ def create_snapshots(project):
         i.wait_until_stopped()
 
         for v in i.volumes.all():
-            print("   Creating snapshot of {0}".format(v.id))
-            v.create_snapshot(Description="Created by Snapshotalyzer")
+            if has_pending_snapshot(v):
+                print("Skipping {0}, snapshot already in progress ".format(v.id))
+                continue
+            else:
+                print("   Creating snapshot of {0}".format(v.id))
+                v.create_snapshot(Description="Created by Snapshotalyzer")
 
         print("Starting {0}...".format(i.id))
 

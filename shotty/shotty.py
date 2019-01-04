73d38898c3dc89625c103f99c2b3d014f651c4b4
unknown <alex.meyer62@gmail.com>
2019-01-04 13:54:10 -0800

    Created try/excepts to handle conflicts with stopping and starting instances

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,4 +1,5 @@
 import boto3
+import botocore
 import click
 
 session = boto3.Session(profile_name='shotty')
@@ -131,7 +132,13 @@ def stop_instances(project):
 
     for i in instances:
         print("Stopping {0}...".format(i.id))
-        i.stop()
+        try:
+            i.stop()
+        except botocore.exceptions.ClientError as e:
+            print("Could not stop {0}. ".format(i.id) + str(e))
+            continue
+
+    return
 
 @instances.command('start')
 @click.option('--project', default=None,
@@ -143,7 +150,12 @@ def start_instances(project):
 
     for i in instances:
         print("Starting {0}...".format(i.id))
-        i.start()
+        try:
+            i.start()  
+        except botocore.exceptions.ClientError as e:
+            print(" Could not start {0}. ".format(i.id) + str(e))
+            continue
+
     return
 
 
